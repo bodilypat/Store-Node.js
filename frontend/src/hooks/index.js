791b9835e3fd0da522ec4919f32d760dcdbf548c
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-03 16:29:45 -0800

    Delete frontend/src directory

--- frontend/src/hooks/index.js
+++
@@ -1,8 +0,0 @@
-//src/hooks/index.js 
-
-export { default as useAuth } from './useAuth';
-export { default as useFetch } from './useFetch';
-export { default as useDebounce } from './useDebounce';
-export { default as useForm } from './useForm';
-export { default as useTable } from './useTable';
-export { default as useInventory } from './useInventory';
