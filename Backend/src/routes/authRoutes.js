9546e06d8ab7661f54e8469cc06afd40f31474c3
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-03-03 16:36:22 -0800

    Create authRoutes.js

---
+++ Backend/src/routes/authRoutes.js
@@ -0,0 +1,16 @@
+//src/routes/authRoutes.js 
+
+const express = require('express');
+const router = express.Router();
+const {
+    register,
+    login,
+    getProfile
+} = require('../controllers/authController');
+const { authenticateToken } = require('../middleware/authMiddleware');
+
+router.post('/register', register);
+router.post('/login', login);
+router.get('/profile', authenticateToken, getProfile);
+
+module.exports = router;
